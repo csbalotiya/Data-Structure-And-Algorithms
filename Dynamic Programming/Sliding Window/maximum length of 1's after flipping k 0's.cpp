f005914349aaf5e13a16cbefb363dbc09ffb3f46
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-04-25 16:00:59 +0530

    Create maximum length of 1's after flipping k 0's.cpp

---
+++ Dynamic Programming/Sliding Window/maximum length of 1's after flipping k 0's.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int arr[] = {0,1,0,1,0,0,1,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int ans = 0 , k = 2;
+    int count0 = 0;
+    int l = 0;
+    for(int i = 0 ; i < n ;i++){
+        if(arr[i] == 0){
+            count0 += 1;
+        }
+
+        while(count0 > k){
+            if(arr[l] == 0)
+                count0-=1;
+            l+=1;
+        }
+        ans = max(ans,i-l+1);
+    }
+    cout<<ans;
+
+  return 0;
+}
