7d3285cce1658e1e65044f34d642a4652a888fe1
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-08-04 22:54:56 +0530

    Create All subsets of a array.cpp

---
+++ Bit magic/All subsets of a array.cpp
@@ -0,0 +1,37 @@
+/*https://leetcode.com/problems/subsets-ii/
+ Subsets II
+
+Solution
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<multiset<int>> res;
+        int n = nums.size();
+        for(int mask=0;mask<(1<<n);mask++){
+            multiset<int> temp;
+            for(int i=0;i<n;i++){
+                if((1<<i)&mask)
+                    temp.insert(nums[i]);
+            }
+            res.insert(temp);
+        }
+        vector<vector<int>> ans;
+        for(auto s:res){
+            vector<int> temp(s.begin(),s.end());
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
