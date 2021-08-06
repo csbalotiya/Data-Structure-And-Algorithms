a64636d6c5f0a86026f7463c26b52b2afa3f5ead
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-08-06 18:13:21 +0530

    Update N-ary Tree Preorder Traversal.cpp

--- Non linear Data Structure/Tree/N-ary Tree Preorder Traversal.cpp
+++ Non linear Data Structure/Tree/N-ary Tree Preorder Traversal.cpp
@@ -9,6 +9,8 @@ Example 1:
 Input: root = [1,null,3,2,4,null,5,6]
 Output: [1,3,5,6,2,4]
 */
+
+//Recursion
 class Solution {
 public:
     void order(Node* root,vector<int>& result) {
@@ -23,3 +25,23 @@ public:
         return v;
     }    
 };
+
+//Iterative Solution:
+
+vector<int> preorder(Node* root) {
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        stack<Node *> stk;
+        stk.push(root);
+        
+        while(!stk.empty()){
+            Node *curr=stk.top();
+            stk.pop();
+            ans.push_back(curr->val);
+            for(int i=curr->children.size()-1;i>=0;i--){
+                stk.push(curr->children[i]);
+            }
+        }
+        return ans;
+    }
