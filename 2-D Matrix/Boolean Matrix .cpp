7926f8cf2ec38df116793412894f38f162de784b
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-08-14 15:11:07 +0530

    Create Boolean Matrix .cpp

---
+++ 2-D Matrix/Boolean Matrix .cpp
@@ -0,0 +1,63 @@
+//https://practice.geeksforgeeks.org/problems/boolean-matrix-problem-1587115620/1#\
+/*
+Example 1:
+
+Input:
+R = 2, C = 2
+matrix[][] = {{1, 0},
+              {0, 0}}
+Output: 
+1 1
+1 0 
+Explanation:
+Only cell that has 1 is at (0,0) so all 
+cells in row 0 are modified to 1 and all 
+cells in column 0 are modified to 1.
+
+Example 2:
+
+Input:
+R = 4, C = 3
+matrix[][] = {{ 1, 0, 0},
+              { 1, 0, 0},
+              { 1, 0, 0},
+              { 0, 0, 0}}
+Output: 
+1 1 1
+1 1 1
+1 1 1
+1 0 0 
+Explanation:
+The position of cells that have 1 in
+the original matrix are (0,0), (1,0)
+and (2,0). Therefore, all cells in row
+0,1,2 are and column 0 are modified to 1. */
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &mat)
+    {
+        int n = mat.size(),m = mat[0].size();
+        int row[n] = {0};
+        int col[m] = {0};
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 1){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    mat[i][j] = 1;
+                }
+            }
+        }
+    }
+};
