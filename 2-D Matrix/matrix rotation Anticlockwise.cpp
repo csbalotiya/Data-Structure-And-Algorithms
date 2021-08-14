ac7317f07344e8ef5cdf6264cf0b563adbb569a3
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-08-14 18:51:11 +0530

    Create matrix rotation Anticlockwise.cpp

---
+++ 2-D Matrix/matrix rotation Anticlockwise.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Anticlock(vector<vector<int>> & arr){
+    int n = arr.size();
+    for(int i = 0; i < n/2; i++){
+        for(int j = i; j <n-i-1; j++){
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][n-j-1];
+            arr[j][n-j-1] = arr[n-j-1][i];
+            arr[n-j-1][i] = temp;
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> arr = { {1,2,3},
+                                {4,5,6},
+                                {7,8,9}};
+
+    Anticlock(arr);
+    int n = arr.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<"\n";
+    }
+}
