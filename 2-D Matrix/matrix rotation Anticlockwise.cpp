f6b5830da131972491519fd0ac9bd9771d3e17d9
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-08-14 19:05:30 +0530

    Update matrix rotation Anticlockwise.cpp

--- 2-D Matrix/matrix rotation Anticlockwise.cpp
+++ 2-D Matrix/matrix rotation Anticlockwise.cpp
@@ -5,8 +5,9 @@ void Anticlock(vector<vector<int>> & arr){
     for(int i = 0; i < n/2; i++){
         for(int j = i; j <n-i-1; j++){
             int temp = arr[i][j];
-            arr[i][j] = arr[j][n-j-1];
-            arr[j][n-j-1] = arr[n-j-1][i];
+            arr[i][j] = arr[j][n-i-1];
+            arr[j][n-i-1] = arr[n-i-1][n-j-1];
+            arr[n-i-1][n-j-1] = arr[n-j-1][i];
             arr[n-j-1][i] = temp;
         }
     }
@@ -25,3 +26,10 @@ int main() {
         }cout<<"\n";
     }
 }
+
+/*
+3 6 9
+2 5 8
+1 4 7
+*/
+
