3c4b0eb753fd688d2a476c5ed8911c2085e0a4b4
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-09-01 19:32:29 +0530

    Update longest Increasing Subseq.cpp

--- Dynamic Programming/longest Increasing Subseq.cpp
+++ Dynamic Programming/longest Increasing Subseq.cpp
@@ -48,3 +48,20 @@ public:
     }
 };
 */
+
+
+//https://www.interviewbit.com/old/problems/longest-increasing-subsequence/
+
+int Solution::lis(const vector<int> &nums) {
+    
+    int n = nums.size();
+    vector<int> ans;
+    for(int i =0 ; i < n; i++){
+        auto idx = lower_bound(ans.begin(), ans.end(), nums[i]) ;
+        if(idx != ans.end())
+            *idx = nums[i];
+        else
+            ans.push_back(nums[i]);
+    }
+    return ans.size();
+}
